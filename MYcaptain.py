8976c97cc5fe3768edc47e7883dbe58ce204a429
josiah2520 <81864900+josiah2520@users.noreply.github.com>
2021-04-03 19:43:37 +0530

    Add files via upload
    THIS program has 2 part
    1st part reads the file extension of the file name given by the user
    2nd part calculates the area of the circle.

--- MYcaptain.py
+++ MYcaptain.py
@@ -1,6 +1,9 @@
-filename = input("Input the Filename: ")
+'''filename = input("Input the Filename: ")
 f_extns = filename.split(".")
 print ("The extension of the file is : " + repr(f_extns[-1]))
-
-
+'''
+pi=3.14
+radius= float(input("ENTER RADIUS OF CIRLCE"))
+area= pi*radius**2
+print("AREA:",area)
             
